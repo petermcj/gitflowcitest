2257f4a37ee0f1e76baec93d9325766cf57534f9
Peter Johansson <petermcj@gmail.com>
2017-04-06 08:04:09 +0200

    inital commit

---
+++ main/java/se/petermcj/maven/minimaven/HelloWorld.java
@@ -0,0 +1,10 @@
+
+package se.petermcj.maven.minimaven;
+
+public class HelloWorld {
+
+  public static String hello(String name){
+    return "Hello " + name;
+  }
+  
+}
---
+++ test/java/se/petermcj/maven/minimaven/HelloWorldIT.java
@@ -0,0 +1,16 @@
+package se.petermcj.maven.minimaven;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldIT {
+  
+  
+  
+  @Test
+  public void helloIT() {
+    assertEquals(HelloWorld.hello("world of IT "), "Hello world  of IT ");
+    
+  }
+  
+}
